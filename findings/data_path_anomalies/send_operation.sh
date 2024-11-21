3116e31eb10e5f74249c4e79d39ccc48f0a084e6
Panhx0601 <Panhx0601@gmail.com>
2024-11-21 11:30:58 +0800

    send operation test

--- findings/data_path_anomalies/send_operation.sh
+++ findings/data_path_anomalies/send_operation.sh
@@ -2,8 +2,8 @@
 
 TAG=$(date +%Y%m%d-%H%M%S)
 
-BINDS="numactl --cpunodebind=2 "
-BINDC="numactl --cpunodebind=2 "
+BINDS="numactl --physcpubind=2 "
+BINDC="numactl --physcpubind=2 "
 
 # server ip
 DEST="phx@115.157.197.8"
@@ -18,12 +18,12 @@ PERFCASE="ib_send_bw"
 SIZE=16
 
 # server side cmd
-SCMD="${PERFCASE} -F --report_gbits -d mlx5_0 -D 1 --run_infinitely -s ${SIZE}"
+SCMD="${PERFCASE} -F --report_gbits --run_infinitely -d mlx5_0 -D 1 -s ${SIZE}"
 
 # client side cmd
-CCMD="/home/phx/MTRDMA/perftest/${PERFCASE} -F --report_gbits -d mlx5_0 -D 1 --run_infinitely -s ${SIZE} ${RNICDEST}"
+CCMD="/home/phx/MTRDMA/perftest/${PERFCASE} -F --report_gbits --run_infinitely -d mlx5_0 -D 1 -s ${SIZE} ${RNICDEST}"
 
 # Execute send operation
-$BINDS $SCMD & ssh $DEST "$BINDC $CCMD " 2>&1 | tee log-$PERFCASE-$TAG.txt
+$BINDS $SCMD > /dev/null & ssh $DEST "timeout 10 $BINDC $CCMD " 2>&1 | tee log-$PERFCASE-$TAG.txt
 
-
+pkill $PERFCASE
